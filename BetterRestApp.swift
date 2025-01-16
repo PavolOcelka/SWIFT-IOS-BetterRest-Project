b756280dc6f1e910d785bad0710ca14a6ab09117
Pavol Ocelka <PavolOcelka>
2025-01-16 11:53:40 +0100

    Initial Commit

---
+++ BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Pavol Ocelka on 16/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
